816b729df02e4c04134f9c89bf62b91647e6530a
theBlueHero <kowalskijohn147@gmail.com>
2018-11-08 16:26:26 -0500

    added some changes to MyFirstTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstTeleOp.java
@@ -2,17 +2,29 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 @TeleOp(name = "TeleOp")
 public class MyFirstTeleOp extends LinearOpMode
+
 {
+    public DcMotor MotorLeft;
+    public DcMotor MotorRight;
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode(){
+
+        MotorLeft = hardwareMap.dcMotor.get("MotorLeft");
+        MotorRight = hardwareMap.dcMotor.get("MotorRight");
+
+        MotorLeft.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
 
         while(opModeIsActive())
         {
+
+            MotorLeft.setPower(-gamepad1.left_stick_y);
+            MotorRight.setPower(-gamepad1.left_stick_y);
 
             idle();
     }
